3d203ba9de3668909da0aa32173891e93b45a776
badrkhamis <khamis_badr@yahoo.ca>
2018-03-14 13:52:07 -0300

    add function data analtic

--- titanic_mainclass.py
+++ titanic_mainclass.py
@@ -6,7 +6,25 @@ class titanic:
     def __init__(self,Train_path,Testpath):
         self.__train = pd.read_csv(Train_path)
         self.__test = pd.read_csv(Testpath)
-        self.__dataset = pd.combine[self.__train,self.__test]
+        # self.__dataset = pd.combine[self.__train,self.__test]
 
     def data_analytic(self):
-        print(self.__dataset.head(5))
+        ''' looking into the data to Analyze, identify patterns, and explore the useful patterns in order
+         to build the prediction model '''
+        '''print(self.__train.head(5)) ; print(self.__train.tail(5))
+        print(self.__train.info())
+        print(self.__train.describe())
+        print(self.__train.describe(include=['O']))
+
+
+        '''
+        print(self.__train.describe())
+
+
+
+
+if __name__ == '__main__':
+    t_path = '/Users/badrkhamis/Documents/ml_experenice/ML_Titanic/dataset/train.csv'
+    te_path = '/Users/badrkhamis/Documents/ml_experenice/ML_Titanic/dataset/test.csv'
+    titanic_p = titanic(t_path,te_path)
+    titanic_p.data_analytic()
