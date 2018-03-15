23e1e844c288e2cd5eb845ef9abf71447ef2eb66
badrkhamis <khamis_badr@yahoo.ca>
2018-03-15 15:41:10 -0300

    preper the data by truncate some columns

--- titanic_mainclass.py
+++ titanic_mainclass.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import numpy as np
+# visualization
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 class titanic:
@@ -20,6 +23,17 @@ class titanic:
         '''
         print(self.__train.describe())
 
+        # anlyzed by pivoting features using the correlation, pivoting features aganist each other
+        #correlation between pclass and survived
+        print(self.__train[['Pclass', 'Survived']].groupby(['Pclass'], as_index=False).mean().sort_values(by='Survived',
+                                                                                                ascending=False))
+        g = sns.FacetGrid(self.__train, col='Survived')
+        g.map(plt.hist, 'Age', bins=20)
+
+
+    def data_preperation(self):
+        self.__train = self.__train.drop(['Ticket','Cabin','Name','PassengerId'], axis=1)
+        print(self.__train.head(5))
 
 
 
@@ -27,4 +41,4 @@ if __name__ == '__main__':
     t_path = '/Users/badrkhamis/Documents/ml_experenice/ML_Titanic/dataset/train.csv'
     te_path = '/Users/badrkhamis/Documents/ml_experenice/ML_Titanic/dataset/test.csv'
     titanic_p = titanic(t_path,te_path)
-    titanic_p.data_analytic()
+    titanic_p.data_preperation()
